80a8ea6fa7748a4a3968ac14060e71ae3ffb7037
Zephneko <zehan.abrar@gmail.com>
2023-09-16 14:19:11 -0400

    Update PostFeed.svelte
    Pushing the scroll container

--- volunteer-together-app/src/PostFeed.svelte
+++ volunteer-together-app/src/PostFeed.svelte
@@ -1,5 +1,48 @@
 <script>
     import MenuBar from './MenuBar.svelte';
+
+    import { onMount } from 'svelte';
+
+    let items = Array(30).fill().map((_, i) => i + 1);
+
+    onMount(() => {
+        const container = document.getElementById('infinite-scroll-container');
+
+        container.addEventListener('scroll', (e) => {
+            if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+                // User has scrolled to the bottom, load more items
+                const lastItem = items[items.length - 1];
+                for (let i = 1; i <= 10; i++) {
+                    items = [...items, lastItem + i];
+                }
+            }
+        });
+    });
+
+    
 </script>
 
 <MenuBar items={[{href: '/dashboardMap', label: 'Map'},{href: '/feed', label: 'Feed'}]} />
+
+
+
+<div id="infinite-scroll-container" class="scroll-container">
+    {#each items as item}
+        <div class="item">{item}</div>
+    {/each}
+</div>
+
+<style>
+    .scroll-container {
+        height: 300px;
+        overflow-y: scroll;
+        border: 1px solid black;
+        width: 200px;
+    }
+
+    .item {
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+    }
+</style>
+
