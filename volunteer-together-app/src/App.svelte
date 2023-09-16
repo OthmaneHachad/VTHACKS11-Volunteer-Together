0ea02c1acbd0441591a31cf390de9dc0efdbc39e
OthmaneHachad <othmanehachad@gmail.com>
2023-09-16 12:40:34 -0400

    google maps api working
    the map for the main page working

--- volunteer-together-app/src/App.svelte
+++ volunteer-together-app/src/App.svelte
@@ -1,14 +1,27 @@
+<svelte:head>
+	<script defer async
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQdXjUnjQHJINv-x9Z_tLpSo7nL7_qo9o&callback=initMap">
+	</script>
+</svelte:head>
+
 <script>
     
 	import {Router, Route} from 'svelte-routing';
 	import Signup from './Signup.svelte';
 	import DashboardMap from './DashboardMap.svelte';
+    import { GoogleAuthProvider } from 'firebase/auth';
 
-
-    
 
 
 </script>
+
+
+
+<style>
+	:global(body) {
+		padding: 0;
+	}
+</style>
 
 
 <Router>
--- volunteer-together-app/src/DashboardMap.svelte
+++ volunteer-together-app/src/DashboardMap.svelte
@@ -1,5 +1,32 @@
+
+
 <script>
+    //import mapStyles from './map-styles'; // optional
+    import { onMount } from 'svelte';
 
-</script>
 
-<h1>The user is redirected !</h1>
+    let container;
+    let map;
+    let zoom = 8;
+    let location = {lat: -34.397, lng: 150.644};
+    
+    
+    
+    onMount(async () => {
+        const {Map} = await google.maps.importLibrary("maps"); 
+        map = new Map(container, {
+            zoom: 4,
+            center: location
+            //styles: mapStyles // optional
+        });
+    });
+    </script>
+
+    <style>
+    .full-screen {
+    width: 100vw;
+    height: 100vh;
+    }
+    </style>
+
+    <div class="full-screen" bind:this={container}></div>
