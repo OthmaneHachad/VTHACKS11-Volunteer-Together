8efc04cacd859b3c5a32e7c43c665fdfe0cc0062
Zephneko <zehan.abrar@gmail.com>
2023-09-17 06:03:24 -0400

    Finished the profile page
    The profile page works now

--- volunteer-together-app/src/firebase.js
+++ volunteer-together-app/src/firebase.js
@@ -3,14 +3,32 @@ console.log("INSIDE firebase.js")
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import {setPersistence, browserLocalPersistence } from 'firebase/auth';
+
+
 
 console.log("imported firebase")
 
 //Your web app's Firebase configuration
 	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+const firebaseConfig = {
+    apiKey: "AIzaSyC_NeDzkny-iC73KLNxPDF2W5wUdnWVQ24",
+    authDomain: "vt-hacks-11-volunteer-together.firebaseapp.com",
+    projectId: "vt-hacks-11-volunteer-together",
+    storageBucket: "vt-hacks-11-volunteer-together.appspot.com",
+    messagingSenderId: "564768052604",
+    appId: "1:564768052604:web:8b91a2ca8bd6789ecb00f9",
+    measurementId: "G-HB9R2HGPS3"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+
+// Set persistence
+setPersistence(auth, browserLocalPersistence);
+
+
+
 export { auth, db };
--- volunteer-together-app/src/profilePage.svelte
+++ volunteer-together-app/src/profilePage.svelte
@@ -1,105 +1,122 @@
 <script>
-    import { collection, setDoc, doc, getDoc, addDoc, query } from 'firebase/firestore';
-   
-    import MenuBar from './MenuBar.svelte';
-    import { onMount } from 'svelte';
-    import { auth, db } from './firebase.js';
+    import { collection, setDoc, doc, getDoc, query } from 'firebase/firestore';
+import MenuBar from './MenuBar.svelte';
+import { onMount, onDestroy } from 'svelte';
+import { auth, db } from './firebase.js';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { writable } from 'svelte/store';
 
-    // Creating a Profile 
-    let firstName = '';
-    let lastName = '';
-    let email = '';
-    let phoneNumber = '';
+let firstName = writable('');
+let lastName = writable('');
+let email = writable('');
+let phoneNumber = writable('');
+let userUid = writable('');
 
-    const userCollection = collection(db, 'users'); // pointing to the 'posts' collection in Firestore
-
-    async function saveProfile() {
-        const newUser = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            phoneNumber: phoneNumber
-        };
-        try {
-            await addDoc(userCollection, newUser);
-            firstName = ''; // reset the title
-            lastName = ''; // reset the content
-            email = ''; // reset the location
-            phoneNumber = "";
-        } catch (e) {
-            console.error("Error adding document to database: ", e);
-        }
-    }
-
-
-
-
-
-
-    let userUid = null; // Initialize userUid variable
-
-    onMount(async () => {
-    // Make sure the user is authenticated
-    if (auth.currentUser) {
-        userUid = auth.currentUser.uid; // Get the UID of the currently authenticated user
-
-        try {
-            // Create a reference to the user's document in Firestore
-            const userDocRef = doc(db, 'users', userUid);
-
-            // Fetch the document
-            const userProfile = await getDoc(userDocRef);
+async function fetchUserProfile(uid) {
+    const userDocRef = doc(db, 'users', uid);
+    const userProfile = await getDoc(userDocRef);
             
-            if (userProfile.exists()) {
-                const data = userProfile.data();
-                firstName = data.firstName;
-                lastName = data.lastName;
-                email = data.email;
-                phoneNumber = data.phoneNumber;
-            } else {
-                console.warn('No profile found for the current user');
-            }
+    if (userProfile.exists()) {
+        const data = userProfile.data();
+        // $userUid = data.userUid || '';
+        $firstName = data.firstName || '';
+        $lastName = data.lastName || '';
+        $email = data.email || '';
+        $phoneNumber = data.phoneNumber || '';
+    } else {
+        console.warn('No profile found for the current user');
+    }
+}
+
+async function saveProfile() {
+    const userUpdate = {
+        firstName: $firstName,
+        lastName: $lastName,
+        email: $email,
+        phoneNumber: $phoneNumber
+    };
+
+    if ($userUid) {
+        try {
+            await setDoc(doc(db, 'users', $userUid), userUpdate, { merge: true });
+            console.log("Profile updated!");
         } catch (e) {
-            console.error("Error fetching profile: ", e);
+            console.error("Error updating profile: ", e);
         }
     } else {
-        console.warn('No user is currently authenticated');
+        console.warn("No UID available for saving.");
     }
+}
+
+let unsubscribe;
+
+onMount(() => {
+    unsubscribe = onAuthStateChanged(auth, user => {
+        if (user) {
+            console.log("Before setting UID:", $userUid);  // Log before setting
+            $userUid = user.uid;
+            console.log("After setting UID:", $userUid);   // Log after setting
+
+            $userUid = user.uid;
+            fetchUserProfile(user.uid);
+        } else {
+            console.warn("No user is currently authenticated.");
+        }
+    });
 });
 
+onDestroy(() => {
+    unsubscribe();
+});
+
+
+
+
+
 </script>
+
 
 <MenuBar items={[{ href: "/dashboardMap", label: "Map" },{ href: "/feed", label: "Feed" }, { href: "/profilePage", label: "Profile" }]}/>
 
 <div class="profile-container">
     <h1>Your Profile</h1>
     
-    <input bind:value={firstName} placeholder="First Name" />
-    <input bind:value={lastName} placeholder="Last Name" />
-    <input bind:value={email} placeholder="Email" />
-    <input bind:value={phoneNumber} placeholder="Phone Number" />
+    <input bind:value={$userUid} readonly />
+    <input bind:value={$firstName} placeholder="First Name" />
+    <input bind:value={$lastName} placeholder="Last Name" />
+    <input bind:value={$email} placeholder="Email" />
+    <input bind:value={$phoneNumber} placeholder="Phone Number" />
 
     <button on:click={saveProfile}>Save Profile</button>
 </div>
 
 <style>
-    
+
+   
+
 
     .profile-container {
         display: flex;
         flex-direction: column;
         gap: 10px;
-        width: 300px;
-        margin: 20px auto;
+        width: 70vh;
+        margin: 30px auto;
         padding: 20px;
         padding-top: 150px;
         border: 1px solid #2e0707;
         border-radius: 5px;
+        height: 50vh; /* This assumes you want to vertically center them on the entire screen. Adjust as needed. */
     }
 
     button {
         align-self: flex-end;
         padding: 5px 10px;
     }
+    input[readonly] {
+    background-color: #f3f3f3;
+    border: 1px solid #ccc;
+    cursor: not-allowed;
+}
+
 
 </style>
